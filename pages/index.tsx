86665118259bea9d980597933a1ca76d5747b988
anhquang <leanhquang.ktpm@gmail.com>
2021-11-25 00:09:07 +0700

    File-system Routing Add New Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
@@ -21,6 +22,10 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <Link href="/promotion">
+          <a>Go to promotion page</a>
+        </Link>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -51,6 +56,13 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
+
+        <div style={{marginTop: '1000px'}}></div>
+        
+        <Link href="/products">
+          <a>Go to products page</a>
+        </Link>
+
       </main>
 
       <footer className={styles.footer}>
