3df7f6e17c6cacea7cdf0c1d72f48a6d52bce72f
anhquang <leanhquang.ktpm@gmail.com>
2021-11-25 23:07:47 +0700

    SSG Getstaticprops Getstaticpaths

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -1,18 +1,63 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import * as React from 'react';
 
 export interface ProductDetailPageProps {
+    movie: any;
 }
 
-export default function App(props: ProductDetailPageProps) {
+export default function App({ movie }: ProductDetailPageProps) {
 
     const params = useRouter();
+
+    if (!movie) return null;
 
     return (
         <div>
             Product Detail Page
 
-            <h3>{JSON.stringify(params.query)}</h3>
+            {/* <h3>{JSON.stringify(params.query)}</h3> */}
+
+            <p>{movie.tenPhim}</p>
+            <p>{movie.trailer}</p>
+            <p>{movie.danhGia}</p>
         </div>
     );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    console.log('Get Static Paths');
+
+    const response = await fetch('https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01');
+    const data = await response.json();
+
+    return {
+        paths: data.map((movie: any) => ({ params: { productId: String(movie.maPhim) } })), //params (type string)
+        fallback: false,
+    }
+}
+
+export const getStaticProps: GetStaticProps<ProductDetailPageProps> = async (context: GetStaticPropsContext) => {
+
+    //server-side
+    //build-time only run (yarn build)
+    //re-run in dev (yarn dev)
+    //support fetch of client-side=
+    //server-side communication client-side forward script(next-data) container data from server => log at client-side
+
+    console.log('Get Static Props', context.params?.productId); //context have contain param at getStaticPaths above
+    const productId = context.params?.productId;
+
+    if (!productId) return {
+        notFound: true,
+    }
+
+    const response = await fetch(`https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=${productId}`);
+    const data = await response.json();
+
+    return {
+        props: {
+            movie: data
+        }
+    }
 }
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,12 +1,46 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import * as React from 'react';
+import Link from 'next/link'
 
 export interface ProductPageProps {
+  movie: any[];
 }
 
-export default function App (props: ProductPageProps) {
+export default function App(props : ProductPageProps) {
+
+  const { movie } = props;
+
+  // console.log('movie', movie);
+  
   return (
     <div>
-        Product Page
+      Product Page
+
+      {
+        movie.map(m => {
+          return <Link key={m.id} href={`/products/${m.id}`}>
+            <a style={{margin: 20}}>{m.name}</a>
+          </Link>
+        })
+      }
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps<ProductPageProps> = async (context : GetStaticPropsContext) => {
+
+  //server-side
+  //build-time only run (yarn build)
+  //re-run in dev (yarn dev)
+  //support fetch of client-side
+  //server-side communication client-side forward script(next-data) container data from server => log at client-side
+
+  const response = await fetch('https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01');
+  const data = await response.json();
+
+  return {
+    props: {
+      movie: data.map((movie: any) => ({id: movie.maPhim, name: movie.tenPhim}))
+    }
+  }
 }
